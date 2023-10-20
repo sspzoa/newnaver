db03168026b51f9a1b44baf1e77c79a3a188279d
Seungpyo Suh <107336329+sspzoa@users.noreply.github.com>
2023-10-20 11:18:08 +0900

    hell

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,6 @@ export default function RootLayout({
       <link rel='icon' href='/src/app/favicon.ico'/>
       <meta name="theme-color" content="#125082"/>
       <meta name="apple-mobile-web-app-status-bar-style" content="#125082"/>
-      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
     </head>
       <body className={inter.className}>{children}</body>
     </html>
